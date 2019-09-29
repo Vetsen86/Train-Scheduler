f83d8cbd91e77312515cedf4dfcb1015949d23ab
Erik Beckstrand <erik.beckstrand@gmail.com>
2019-09-29 02:12:07 -0600

    add function to calculate train times

--- assets/java/app.js
+++ assets/java/app.js
@@ -2,18 +2,38 @@ $(document).ready(function() {
 
     var database = firebase.database();
 
+    var arrivalTime;
+    var minutesAway;
+
+    function calculateTime(time, frequency) {
+        var timeConverted = moment(time, "HH:mm").subtract(1, "years");
+        console.log(timeConverted);
+
+        var diffTime = moment().diff(moment(timeConverted), "minutes");
+        console.log("Difference: " + diffTime);
+
+        var tRemainder = diffTime % frequency;
+
+        minutesAway = frequency - tRemainder;
+
+        arrivalTime = moment().add(minutesAway, "minutes");
+        arrivalTime = moment(arrivalTime).format("hh:mm");
+    }
+
     function renderTrains() {
         var name = $("#train-name").val().trim();
         var destination = $("#destination").val().trim();
         var frequency = $("#frequency").val();
         var time = $("#train-time").val();
 
+        calculateTime(time, frequency);
+
         var newRow = $("<tr>");
         var nameData = $("<td>").text(name);
         var destinationData = $("<td>").text(destination);
         var frequencyData = $("<td>").text(frequency);
-        var nextArrivalData = $("<td>").text("0");
-        var awayData = $("<td>").text("0");
+        var nextArrivalData = $("<td>").text(arrivalTime);
+        var awayData = $("<td>").text(minutesAway);
 
         newRow.append(nameData);
         newRow.append(destinationData);
@@ -35,6 +55,7 @@ $(document).ready(function() {
     $("#submit").on("click", function(e) {
         e.preventDefault();
         renderTrains();
+        $("input").val("");
     });
 
 });
